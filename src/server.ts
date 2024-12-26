37eb0f27d3e28e9c8aea51b853433143423c4ee0
Tanishq Agarwal <tanishqagarwal5414@gmail.com>
2024-12-26 20:24:09 +0530

    fixed: cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
